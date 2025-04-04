110e28b466d3c4528e8a4db1e74472acb7d755b1
Tarasov Petr <patarasov@edu.hse.ru>
2025-04-04 15:28:24 +0300

    farfetch parser, notebook with building embedding model, PineCone main logic

---
+++ PineCone/Dataset.py
@@ -0,0 +1,28 @@
+from torch.utils.data import Dataset
+from torchvision import transforms
+
+import pandas as pd
+from PIL import Image
+
+
+class ClothesDataset(Dataset):
+    def __init__(self, dataset_path, dataset_base):
+        self.dataset_base = dataset_base
+        self.data = pd.read_csv(dataset_path)
+        self.data.iloc[:, -1] = self.data.iloc[:, -1].apply(lambda x: eval(x)[0])
+        self.transform = transforms.Compose([
+            transforms.Resize((224, 224)),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+        ])
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        brand = self.data.iloc[idx, 0]
+        name = self.data.iloc[idx, 1]
+        path = self.data.iloc[idx, 2]
+        image = Image.open(self.dataset_base + path).convert("RGB")
+        image = self.transform(image).unsqueeze(0)
+        return idx, brand, name, image
---
+++ PineCone/PineconeInit.py
@@ -0,0 +1,50 @@
+import os
+from dotenv import load_dotenv
+
+from Dataset import ClothesDataset
+
+import torch
+from torch.utils.data import DataLoader
+import torch.multiprocessing as mp
+
+from tqdm import tqdm
+
+from EmbeddingModel import ResNetEmbedding
+
+from pinecone import Pinecone, ServerlessSpec
+
+
+load_dotenv()
+
+API_KEY = os.getenv("PINECONE_API_KEY")
+DATASET_PATH = os.getenv("DATASET_PATH")
+WEIGHTS_PATH = os.getenv("WEIGHTS_PATH")
+
+pc = Pinecone(api_key=API_KEY)
+
+mp.set_start_method('fork', force=True)
+dataset = ClothesDataset(DATASET_PATH, r"parsers/dataset/")
+loader = DataLoader(dataset, batch_size=16, shuffle=False, num_workers=3)
+
+device = torch.device("mps")
+embedding_model = ResNetEmbedding()
+checkpoint = torch.load(WEIGHTS_PATH, map_location=device)
+embedding_model.load_state_dict(checkpoint["model_state_dict"])
+embedding_model.to(device)
+embedding_model.eval()
+
+index = pc.Index("clothes-images")
+
+progress_bar = tqdm(loader, desc="Buildimg embeddings")
+for batch in progress_bar:
+    vectors = []
+    for idx, brand, name, image in zip(*batch):
+        image = image.to(device)
+        embedding = embedding_model(image).cpu().detach().numpy()
+        vectors.append({
+            "id": str(idx.item()),
+            "values": embedding.tolist()[0],
+            "metadata": {"brand": brand, "name": name}
+        })
+    index.upsert(vectors=vectors, namespace="clothes-images")
+
